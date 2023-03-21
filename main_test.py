beba56d73d4108aa0bdd8704dc586bf1468b3ff5
Nicholas Davidson <59320455+ndavidson19@users.noreply.github.com>
2023-03-20 22:02:04 -0700

    Added Testing Suite

---
+++ main_test.py
@@ -0,0 +1,15 @@
+from main import coin_toss, weighted_coin_toss
+
+def test_coin_toss():
+    # Test that the coin toss function returns a tuple
+    assert isinstance(coin_toss(1000), tuple)
+
+def test_coin_toss_length():
+    # Test that the coin toss function returns a tuple of length 2
+    assert len(coin_toss(1000)) == 2
+
+def test_coin_toss_values():
+    # Test that the coin toss function returns a tuple of integers
+    assert isinstance(coin_toss(1000)[0], int)
+    assert isinstance(coin_toss(1000)[1], int)
+
